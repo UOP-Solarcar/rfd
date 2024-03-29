946d777efc9369cafb49c15c7000fc1057c3517a
Ngoc Gia Han Nguyen <n_nguyen134@u.pacific.edu>
2024-03-28 18:32:28 -0700

    modified pyRead.py, it's working

--- pyRead.py
+++ pyRead.py
@@ -1,10 +1,8 @@
 import serial
 
-with serial.Serial('/dev/ttyUSB1', timeout=10) as ser:
-    ser.baudrate = 57600
-    print(ser.name) 
-    line = ser.readline()   # read a '\n' terminated line
-    print(line.decode().strip())
-    # for p in line:
-    #     print(p)
+ser = serial.Serial('/dev/tty.usbserial-B000B7O7') #, timeout=10)
+ser.baudrate = 57600
+print(ser.name) 
+line = ser.readline()   # read a '\n' terminated line
+print(line.decode().strip())
 
--- pySend.py
+++ pySend.py
@@ -1,5 +1,5 @@
 import serial
-ser = serial.Serial('/dev/ttyUSB0')  # open serial port
+ser = serial.Serial('/dev/tty.usbserial-B000B7O7')  # open serial port
 ser.baudrate = 57600
 ser.flush()
 print(ser.name)         # check which port was really used
