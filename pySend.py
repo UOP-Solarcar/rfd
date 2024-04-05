22f347ff63cef7e3ceb3be439548de68996c3bcc
Ngoc Gia Han Nguyen <n_nguyen134@u.pacific.edu>
2024-04-04 18:08:36 -0700

    Take 3rd arg as port_name

--- pySend.py
+++ pySend.py
@@ -3,15 +3,24 @@ import json
 import sys
 
 # File I/O
+port_name = ''
+
 if(len(sys.argv) == 1):
     # default file
     filename = "data.json"
 elif(len(sys.argv) == 2):
     # input file
     filename = sys.argv[1]
+elif(len(sys.argv) == 3):
+    # arg 3 as port_name but it is optional
+    filename = sys.argv[1]
+    port_name = sys.argv[2]
 else:
     print("ERROR: INCORRECT INPUT. python3 pySend.py [input_file.json (optional)]")
     exit()
+
+
+
 with open(filename) as input_file:
     try: 
         data = json.load(input_file)
@@ -21,7 +30,7 @@ with open(filename) as input_file:
 # Convert File from file to string, add \n to signal end
 packet = json.dumps(data) + '\n'
 
-ser = serial.Serial('COM10')  # open serial port
+ser = serial.Serial(port_name)  # open serial port
 ser.baudrate = 57600
 ser.flush() 
 print(ser.name)         # check which port was really used
