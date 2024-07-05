8047204f71d4d7cad58067c95e0b74387edf954a
MiguelX413 <lbpgaming27@gmail.com>
2024-07-04 19:37:05 -0700

    Reformat py_read.py and py_send.py and create requirements.txt

--- py_send.py
+++ py_send.py
@@ -1,40 +1,47 @@
+#!/usr/bin/env python3
+
 import serial
 import json
 import sys
 
-# File I/O
-port_name = ""
 
-if len(sys.argv) == 1:
-    # default file
-    filename = "data.json"
-elif len(sys.argv) == 2:
-    # input file
-    filename = sys.argv[1]
-elif len(sys.argv) == 3:
-    # arg 3 as port_name but it is optional
-    filename = sys.argv[1]
-    port_name = sys.argv[2]
-else:
-    print("ERROR: INCORRECT INPUT. python3 pySend.py [input_file.json (optional)]")
-    exit()
+def main() -> None:
+    # File I/O
+    port_name = ""
 
-
-with open(filename) as input_file:
-    try:
-        data = json.load(input_file)
-    except:
-        print("Error opening file: ", input_file)
+    if len(sys.argv) == 1:
+        # default file
+        filename = "data.json"
+    elif len(sys.argv) == 2:
+        # input file
+        filename = sys.argv[1]
+    elif len(sys.argv) == 3:
+        # arg 3 as port_name but it is optional
+        filename = sys.argv[1]
+        port_name = sys.argv[2]
+    else:
+        print("ERROR: INCORRECT INPUT. python3 pySend.py [input_file.json (optional)]")
         exit()
-# Convert File from file to string, add \n to signal end
-packet = json.dumps(data) + "\n"
 
-ser = serial.Serial(port_name)  # open serial port
-ser.baudrate = 57600
-ser.flush()
-print(ser.name)  # check which port was really used
-print(type(packet))
-s = ser.write(packet.encode())  # write a string
-print(packet)
-print("Size, ", s)
-ser.close()
+    with open(filename) as input_file:
+        try:
+            data = json.load(input_file)
+        except:
+            print("Error opening file: ", input_file)
+            exit()
+    # Convert File from file to string, add \n to signal end
+    packet = json.dumps(data) + "\n"
+
+    ser = serial.Serial(port_name)  # open serial port
+    ser.baudrate = 57600
+    ser.flush()
+    print(ser.name)  # check which port was really used
+    print(type(packet))
+    s = ser.write(packet.encode())  # write a string
+    print(packet)
+    print("Size, ", s)
+    ser.close()
+
+
+if __name__ == "__main__":
+    main()
